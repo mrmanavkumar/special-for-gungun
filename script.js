30070a6eb431324f723f277ada11a99c691b2ad5
mrmanavkumar <mrmanavkumar869@gmail.com>
2026-09-19 10:57:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     // DOM Elements
     const giftSection = document.getElementById("giftSection");
     const mainLink = document.getElementById("mainLink");
@@ -488,4 +488,4 @@ Document.addEventListener("DOMContentLoaded", () => {
         draw();
     }
 });
-                                  
+    
